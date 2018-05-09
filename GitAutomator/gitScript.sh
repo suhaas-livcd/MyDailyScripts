b840ab3b32eb28e73ac00689dd24b4c0cf13a029
suhaas <suhaas95@gmail.com>
2018-05-09 08:08:30 +0530

    [Initial Commit]

---
+++ GitAutomator/gitScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# declare STRING variable
+STRING="Hello World"
+#print variable on a screen
+echo $STRING
+#PWD : /home/suhaas/Desktop/MyPrepLogger/diary
+CURRENT_DATE="`date "+%d_%m_%Y_%H:%M:%S"`"
+echo $CURRENT_DATE
